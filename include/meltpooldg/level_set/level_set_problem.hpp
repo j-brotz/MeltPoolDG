31c8a1941b11dd5705ca26f5225d23a76a783de4
Peter Munch <peterrmuench@gmail.com>
2021-04-03 08:24:01 +0200

    Pass communicator to ScratchData

--- include/meltpooldg/level_set/level_set_problem.hpp
+++ include/meltpooldg/level_set/level_set_problem.hpp
@@ -98,7 +98,8 @@ namespace MeltPoolDG
         /*
          *  setup scratch data
          */
-        scratch_data = std::make_shared<ScratchData<dim>>(base_in->parameters.base.verbosity_level,
+        scratch_data = std::make_shared<ScratchData<dim>>(base_in->mpi_communicator,
+                                                          base_in->parameters.base.verbosity_level,
                                                           /* do_matrix_free */ true);
         /*
          *  setup mapping
