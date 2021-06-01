a13b738ac70a124a080f328e350bae917c55f4c1
Magdalena Schreter <schreter.magdalena@gmail.com>
2021-06-01 10:38:47 +0200

    add docu base class

--- include/meltpooldg/evaporation/evaporation_model_base.hpp
+++ include/meltpooldg/evaporation/evaporation_model_base.hpp
@@ -7,9 +7,18 @@
 
 namespace MeltPoolDG::Evaporation
 {
+  /**
+   * Base class for implementing different models to compute the evaporative
+   * mass flux.
+   */
   class EvaporationModelBase
   {
   public:
+    /**
+     *                                                    .
+     * Base function to compute the evaporative mass flux m in kg/(m^2 s) for
+     * a given temperature value @p T.
+     */
     virtual double
     local_compute_evaporative_mass_flux(const double T) = 0;
   };
