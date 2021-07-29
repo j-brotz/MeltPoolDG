a5b2a7a90f43604e0677cd9a7156ce2e11c8d426
Magdalena Schreter <schreter.magdalena@gmail.com>
2021-07-29 20:55:36 +0200

    curv filter term: c*h --> c*h^2

--- source/curvature/curvature_operation.cpp
+++ source/curvature/curvature_operation.cpp
@@ -124,8 +124,8 @@ namespace MeltPoolDG::Curvature
   void
   CurvatureOperation<dim>::create_operator(const VectorType &solution_levelset)
   {
-    const double damping_parameter =
-      scratch_data->get_min_cell_size(curv_dof_idx) * curvature_data.damping_scale_factor;
+    const double damping_parameter = std::pow(scratch_data->get_min_cell_size(curv_dof_idx), 2) *
+                                     curvature_data.damping_scale_factor;
 
     curvature_operator = std::make_unique<CurvatureOperator<dim>>(*scratch_data,
                                                                   damping_parameter,
