3efda459dae8e1ac3a7060d6e065bbaae0a503b1
Peter Munch <peterrmuench@gmail.com>
2021-10-19 13:14:35 +0200

    Fix typo

--- include/meltpooldg/heat/heat_transfer_operator.hpp
+++ include/meltpooldg/heat/heat_transfer_operator.hpp
@@ -152,14 +152,14 @@ namespace MeltPoolDG::Heat
                          const MaterialData<number> &                    material_data_in,
                          const unsigned int                              temp_dof_idx_in,
                          const unsigned int                              temp_quad_idx_in,
-                         const unsigned int temp_hanging_nodes_dof_idxdof_idx_in,
-                         const VectorType & temperature_in,
-                         const VectorType & temperature_old_in,
-                         const VectorType & heat_source_in,
-                         const unsigned int vel_dof_idx_in            = 0,
-                         const VectorType * velocity_in               = nullptr,
-                         const unsigned int ls_dof_idx_in             = 0,
-                         const VectorType * level_set_as_heaviside_in = nullptr);
+                         const unsigned int                              temp_hanging_nodes_dof_idx,
+                         const VectorType &                              temperature_in,
+                         const VectorType &                              temperature_old_in,
+                         const VectorType &                              heat_source_in,
+                         const unsigned int                              vel_dof_idx_in = 0,
+                         const VectorType *                              velocity_in    = nullptr,
+                         const unsigned int                              ls_dof_idx_in  = 0,
+                         const VectorType *level_set_as_heaviside_in                    = nullptr);
 
     void
     register_evaporative_mass_flux(VectorType *       evaporative_mass_flux_in,
