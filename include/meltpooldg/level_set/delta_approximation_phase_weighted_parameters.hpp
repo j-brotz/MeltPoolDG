e579262a213cb0e81b5dea7a56eb5668ccdf3027
Nils Much <nils.much@tum.de>
2021-12-16 09:29:48 +0100

    Refer to class instead of describing the deltas twice

--- include/meltpooldg/level_set/delta_approximation_phase_weighted_parameters.hpp
+++ include/meltpooldg/level_set/delta_approximation_phase_weighted_parameters.hpp
@@ -10,22 +10,12 @@ namespace MeltPoolDG
     char,
     norm_of_indicator_gradient, // use δ = ||∇ϕ|| as approximation for the Dirac delta function
     // with the heaviside representation of the level set ϕ
-    phase_weighted_delta, // approximate the Dirac delta function with
-                          // δ = ||∇ϕ|| ( w_g (1-ϕ) + w_h ϕ ) 2 / ( w_g + w_h )
-                          // with the weight of the gas phase w_g and the weight of the heavy phase
-                          // w_h
-    // with the heaviside representation of the level set ϕ
-    quad_phase_weighted_delta, // approximate the Dirac delta function with
-                               // δ = ||∇ϕ|| ( w_g (1-ϕ) + w_h ϕ )² 3 / ( w_g² + w_g w_h + w_h² )
-                               // with the weight of the gas phase w_g and the weight of the heavy
-                               // phase w_h
-    double_phase_weighted_delta, // approximate the Dirac delta function with
-                                 //      6 ||∇ϕ|| ( w_1g (1-ϕ) + w_1h ϕ )( w_2g (1-ϕ) + w_2h ϕ )
-                                 // δ = ---------------------------------------------------------
-                                 //         2 w_1g w_2g + w_1g w_2h + w_1h w_2g + 2 w_1h w_2h
-                                 // with the weights of the gas phase w_1g and w_2g and the weights
-                                 // of the heavy phase w_1h and w_2h
-    delta_weighted_consistent_with_evaporation)
+    phase_weighted_delta,        // see DeltaApproximationPhaseWeighted
+    quad_phase_weighted_delta,   // see DeltaApproximationQuadPhaseWeighted
+    double_phase_weighted_delta, // see DeltaApproximationDoublePhaseWeighted
+    delta_weighted_consistent_with_evaporation
+    // see DeltaApproximationPhaseWeightedConsistentWithEvaporation
+  )
 
   template <typename number = double>
   struct DeltaApproximationPhaseWeightedData
