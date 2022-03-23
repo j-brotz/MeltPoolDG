0fa7934d3c7ccf5394919940a2fac0662ac368aa
Nils Much <nils.much@tum.de>
2022-03-23 16:36:30 +0100

    Fix: compute evpor mass flux also for non-explicit evaporation

--- source/melt_pool/melt_pool_problem.cpp
+++ source/melt_pool/melt_pool_problem.cpp
@@ -33,28 +33,35 @@ namespace MeltPoolDG::MeltPool
         scratch_data->initialize_dof_vector(interface_velocity, vel_dof_idx);
         interface_velocity.copy_locally_owned_data_from(flow_operation->get_velocity());
 
-        if (evaporation_operation && problem_specific_parameters.do_evaporative_mass_flux)
+        if (evaporation_operation)
           {
             TimerOutput::Scope scope(scratch_data->get_timer(), "Evaporation::mass_flux");
-            switch (base_in->parameters.evapor.level_set_source_term_type)
-              {
-                default:
-                case EvaporationLevelSetSourceTermType::interface_velocity:
-                  // Option 1: compute modified advection velocity due to evaporation
-                  evaporation_operation->compute_evaporation_velocity();
-                  interface_velocity += evaporation_operation->get_velocity();
-                  break;
 
-                case EvaporationLevelSetSourceTermType::rhs:
-                  // Option 2: use source term as rhs in the level set equation
-                  scratch_data->initialize_dof_vector(level_set_rhs, ls_dof_idx);
-                  evaporation_operation->compute_level_set_source_term(
-                    level_set_rhs,
-                    ls_dof_idx,
-                    level_set_operation.get_level_set(),
-                    pressure_dof_idx);
-                  level_set_operation.set_level_set_user_rhs(level_set_rhs);
-                  break;
+            // compute the evaporative mass flux
+            evaporation_operation->compute_evaporative_mass_flux();
+
+            if (problem_specific_parameters.do_evaporative_mass_flux)
+              {
+                switch (base_in->parameters.evapor.level_set_source_term_type)
+                  {
+                    default:
+                    case EvaporationLevelSetSourceTermType::interface_velocity:
+                      // Option 1: compute modified advection velocity due to evaporation
+                      evaporation_operation->compute_evaporation_velocity();
+                      interface_velocity += evaporation_operation->get_velocity();
+                      break;
+
+                    case EvaporationLevelSetSourceTermType::rhs:
+                      // Option 2: use source term as rhs in the level set equation
+                      scratch_data->initialize_dof_vector(level_set_rhs, ls_dof_idx);
+                      evaporation_operation->compute_level_set_source_term(
+                        level_set_rhs,
+                        ls_dof_idx,
+                        level_set_operation.get_level_set(),
+                        pressure_dof_idx);
+                      level_set_operation.set_level_set_user_rhs(level_set_rhs);
+                      break;
+                  }
               }
           }
 
@@ -127,9 +134,6 @@ namespace MeltPoolDG::MeltPool
           // .... d) evaporative mass fluxes
           if (evaporation_operation && problem_specific_parameters.do_evaporative_mass_flux)
             {
-              // compute the evaporative mass flux
-              evaporation_operation->compute_evaporative_mass_flux();
-
               evaporation_operation->compute_mass_balance_source_term(
                 mass_balance_rhs,
                 flow_operation->get_dof_handler_idx_pressure(),
