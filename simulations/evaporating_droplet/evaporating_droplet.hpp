db9ca9891b7465ceee38f599620c15d2b463ee88
Magdalena Schreter <schreter.magdalena@gmail.com>
2022-04-24 21:16:31 +0200

    add simulation specific parameters to evaporating droplet

--- simulations/evaporating_droplet/evaporating_droplet.hpp
+++ simulations/evaporating_droplet/evaporating_droplet.hpp
@@ -10,9 +10,9 @@
 #include <deal.II/distributed/tria.h>
 
 #include <deal.II/grid/grid_generator.h>
-#include <deal.II/grid/manifold_lib.h>
 
 #include <iostream>
+
 // MeltPoolDG
 #include <meltpooldg/interface/simulation_base.hpp>
 
@@ -24,13 +24,20 @@ namespace MeltPoolDG
     {
       using namespace dealii;
 
+      static double droplet_radius = 0.5;
+      static double domain_length  = 4;
+
+      BETTER_ENUM(DomainType, char, rectangle, ball)
+
+      static DomainType domain_type = DomainType::rectangle;
+
       template <int dim>
       class InitialValuesLS : public Function<dim>
       {
       public:
         InitialValuesLS(const double eps)
           : Function<dim>()
-          , distance_sphere(dim == 2 ? Point<dim>(2, 2) : Point<dim>(2, 2, 2), 0.5)
+          , distance_sphere(Point<dim>(), droplet_radius)
           , eps(eps)
         {}
 
@@ -46,10 +53,6 @@ namespace MeltPoolDG
         const double                                 eps;
       };
 
-      /*
-       *      This class collects all relevant input data for the level set simulation
-       */
-
       template <int dim>
       class SimulationEvaporatingDroplet : public SimulationBase<dim>
       {
@@ -59,79 +62,82 @@ namespace MeltPoolDG
         {}
 
         void
+        add_simulation_specific_parameters(dealii::ParameterHandler &prm) override
+        {
+          prm.enter_subsection("simulation specific");
+          {
+            prm.add_parameter("droplet radius", droplet_radius, "Set the radius of the droplet.");
+            prm.add_parameter("domain length",
+                              domain_length,
+                              "Set the characteristic length of the domain.");
+            prm.add_parameter("domain type", domain_type, "Set the type of the domain.");
+          }
+          prm.leave_subsection();
+        }
+
+        void
         create_spatial_discretization() override
         {
-          if (this->parameters.base.do_simplex)
-            {
-              this->triangulation =
-                std::make_shared<parallel::shared::Triangulation<dim>>(this->mpi_communicator);
-            }
+          // create triangulation
+          if (this->parameters.base.do_simplex || dim == 1)
+            this->triangulation =
+              std::make_shared<parallel::shared::Triangulation<dim>>(this->mpi_communicator);
           else
-            {
-              this->triangulation =
-                std::make_shared<parallel::distributed::Triangulation<dim>>(this->mpi_communicator);
-            }
+            this->triangulation =
+              std::make_shared<parallel::distributed::Triangulation<dim>>(this->mpi_communicator);
 
-          if constexpr ((dim == 2) || (dim == 3))
+          const double half_length = domain_length / 2.;
+
+          AssertThrow(droplet_radius < half_length,
+                      ExcMessage(
+                        "The droplet radius exceeds the domain size. "
+                        "You might adjust either the domain length or the droplet radius."));
+
+          if (domain_type == DomainType::rectangle)
             {
-              // create mesh
               std::vector<unsigned int> subdivisions(
                 dim,
                 5 * (this->parameters.base.do_simplex ?
                        Utilities::pow(2, this->parameters.base.global_refinements) :
                        1));
 
-              const Point<dim> bottom_left;
-              const Point<dim> top_right = (dim == 2 ? Point<dim>(4, 4) : Point<dim>(4, 4, 4));
+              const Point<dim> bottom_left =
+                (dim == 1 ? Point<dim>(-half_length) :
+                 dim == 2 ? Point<dim>(-half_length, -half_length) :
+                            Point<dim>(-half_length, -half_length, -half_length));
+              const Point<dim> top_right =
+                (dim == 1 ? Point<dim>(half_length) :
+                 dim == 2 ? Point<dim>(half_length, half_length) :
+                            Point<dim>(half_length, half_length, half_length));
 
               if (this->parameters.base.do_simplex)
-                {
-                  GridGenerator::subdivided_hyper_rectangle_with_simplices(*this->triangulation,
-                                                                           subdivisions,
-                                                                           bottom_left,
-                                                                           top_right);
-                }
+                GridGenerator::subdivided_hyper_rectangle_with_simplices(*this->triangulation,
+                                                                         subdivisions,
+                                                                         bottom_left,
+                                                                         top_right);
               else
-                {
-                  GridGenerator::subdivided_hyper_rectangle(*this->triangulation,
-                                                            subdivisions,
-                                                            bottom_left,
-                                                            top_right);
-                }
-
-              // set boundary indicator to 2 on left and right face -> symmetry boundary
-              // for (const auto &cell : this->triangulation->cell_iterators())
-              // for (unsigned int face = 0; face < cell->n_faces(); ++face)
-              // if (cell->face(face)->at_boundary() &&
-              //(std::fabs(cell->face(face)->center()[0]) < 1e-14 ||
-              // std::fabs(cell->face(face)->center()[1]) < 1e-14))
-              // cell->face(face)->set_boundary_id(2);
-              // else if (cell->face(face)->at_boundary())
-              // cell->face(face)->set_boundary_id(1);
-
-              if (this->parameters.base.do_simplex == false)
-                this->triangulation->refine_global(this->parameters.base.global_refinements);
+                GridGenerator::subdivided_hyper_rectangle(*this->triangulation,
+                                                          subdivisions,
+                                                          bottom_left,
+                                                          top_right);
+            }
+          else if (domain_type == DomainType::ball)
+            {
+              // create circular domain
+              const Point<dim> center;
+              GridGenerator::hyper_ball(*this->triangulation, center, half_length /*radius*/);
             }
           else
-            {
-              AssertThrow(false, ExcNotImplemented());
-            }
+            AssertThrow(false, ExcNotImplemented());
+
+          if (this->parameters.base.do_simplex == false)
+            this->triangulation->refine_global(this->parameters.base.global_refinements);
         }
 
         void
         set_boundary_conditions() override
         {
-          // auto dirichlet = std::make_shared<Functions::ConstantFunction<dim>>(-1.0);
-
-          // lower, right and left faces
-          // this->attach_no_slip_boundary_condition(0, "navier_stokes_u");
-          //// upper face
-          // this->attach_symmetry_boundary_condition(2, "navier_stokes_u");
-          // this->attach_no_slip_boundary_condition(1, "navier_stokes_u");
-
-          // this->attach_dirichlet_boundary_condition(0, dirichlet, "level_set");
-          // this->attach_dirichlet_boundary_condition(2, dirichlet, "level_set");
-
+          // zero normal stress component
           this->attach_open_boundary_condition(0, "navier_stokes_u");
         }
 
