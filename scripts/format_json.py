1055f6b7a5789a8c44825709b0ffab111005ba91
Magdalena Schreter <schreter.magdalena@gmail.com>
2022-05-12 08:03:13 +0200

    fix formatting

--- scripts/format_json.py
+++ scripts/format_json.py
@@ -3,23 +3,26 @@ from argparse import ArgumentParser
 import collections
 import json
 
+
 def parseArguments():
     parser = ArgumentParser(description="Format json file.")
 
-    parser.add_argument('file_in' , help="Input file.")
-    parser.add_argument('file_out' , help="Output file.")
+    parser.add_argument('file_in', help="Input file.")
+    parser.add_argument('file_out', help="Output file.")
 
     arguments = parser.parse_args()
     return arguments
 
+
 def main():
-  args = parseArguments()
+    args = parseArguments()
 
-  with open(args.file_in, 'r') as f:
-    datastore = json.load(f, object_pairs_hook=collections.OrderedDict)
+    with open(args.file_in, 'r') as f:
+        datastore = json.load(f, object_pairs_hook=collections.OrderedDict)
 
-  with open(args.file_out, 'w') as f:
-    json.dump(datastore, f, indent=2, separators=(',', ': '))
+    with open(args.file_out, 'w') as f:
+        json.dump(datastore, f, indent=2, separators=(',', ': '))
 
-if __name__== "__main__":
-  main()
+
+if __name__ == "__main__":
+    main()
