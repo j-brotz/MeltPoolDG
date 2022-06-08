f96fe9f7a63151ca766f1d7cd6e47d1401c4bc48
Magdalena Schreter <schreter.magdalena@gmail.com>
2022-06-08 16:04:33 +0200

    fix invalid_double

--- include/meltpooldg/utilities/numbers.hpp
+++ include/meltpooldg/utilities/numbers.hpp
@@ -9,12 +9,20 @@
 
 namespace dealii::numbers
 {
-  constexpr double invalid_double = std::numeric_limits<double>::min();
+  // @note: We did not use std::numeric_limits<double>::lowest() on purpose.
+  // @p invalid_double should serve as a default value for an optional parameter.
+  // During parsing, the ParameterHandler converts the default value (e.g.
+  // set to invalid_double) to a string via Patterns::Tools::to_string<double>()
+  // and then back to a double value. This entails a loss in the number of decimal
+  // places and thus round-off errors.
+  const double invalid_double = -1e100;
 
-  constexpr auto
-  is_invalid(double number)
+  // Check if a @p number is invalid, if @p number is smaller or equal to
+  // numbers::invalid_double.
+  inline bool
+  is_invalid(const double &number)
   {
-    return number == invalid_double;
+    return number <= invalid_double;
   }
 
 } // namespace dealii::numbers
