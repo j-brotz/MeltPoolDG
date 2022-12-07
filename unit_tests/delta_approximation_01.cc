55941bb16971b1dff0e7b939116a857f291b2d13
Magdalena Schreter <schreter.magdalena@gmail.com>
2022-12-07 21:02:11 +0100

    complete unit test

--- unit_tests/delta_approximation_01.cc
+++ unit_tests/delta_approximation_01.cc
@@ -1,6 +1,7 @@
 // Test delta-approximation classes.
 
 #include <meltpooldg/level_set/delta_approximation_phase_weighted.hpp>
+#include <meltpooldg/material/material_data.hpp>
 
 #include <iostream>
 
@@ -9,9 +10,14 @@ using namespace dealii;
 
 template <typename Number>
 void
-test(const DeltaApproximationPhaseWeightedData<Number> &data, const unsigned int n_intervals = 100)
+test(const DeltaApproximationPhaseWeightedData<Number> data, const unsigned int n_intervals = 100)
 {
-  const auto delta_approximation = create_phase_weighted_delta_approximation(data);
+  const std::unique_ptr<DeltaApproximationBase<Number>> delta_approximation =
+    create_phase_weighted_delta_approximation(data);
+
+  // norm of indicator gradient
+  if (!delta_approximation)
+    return;
 
   std::cout << data.type << ": " << std::endl;
   for (unsigned int i = 0; i <= n_intervals; ++i)
@@ -19,52 +25,22 @@ test(const DeltaApproximationPhaseWeightedData<Number> &data, const unsigned int
       const auto f = static_cast<Number>(i) / static_cast<Number>(n_intervals);
       std::cout << delta_approximation->compute_weight(f) << " ";
     }
+
   std::cout << std::endl << std::endl;
 }
 
 int
 main()
 {
-  {
-    DeltaApproximationPhaseWeightedData data;
-    data.type = DiracDeltaFunctionApproximationType::heaviside_phase_weighted;
-    test(data);
-  }
+  DeltaApproximationPhaseWeightedData data;
+  data.gas_phase_weight     = 0.1;
+  data.heavy_phase_weight   = 0.2;
+  data.gas_phase_weight_2   = 0.5;
+  data.heavy_phase_weight_2 = 1.0;
 
-  {
-    DeltaApproximationPhaseWeightedData data;
-    data.type = DiracDeltaFunctionApproximationType::heavy_phase_only;
-    test(data);
-  }
-
-  {
-    DeltaApproximationPhaseWeightedData data;
-    data.type = DiracDeltaFunctionApproximationType::quad_heaviside_phase_weighted;
-    test(data);
-  }
-
-  {
-    DeltaApproximationPhaseWeightedData data;
-    data.type = DiracDeltaFunctionApproximationType::heaviside_times_heaviside_phase_weighted;
-    test(data);
-  }
-
-  {
-    DeltaApproximationPhaseWeightedData data;
-    data.type               = DiracDeltaFunctionApproximationType::reciprocal_phase_weighted;
-    data.gas_phase_weight   = 0.1;
-    data.heavy_phase_weight = 0.2;
-    test(data);
-  }
-
-  {
-    DeltaApproximationPhaseWeightedData data;
-    data.gas_phase_weight     = 0.1;
-    data.heavy_phase_weight   = 0.2;
-    data.gas_phase_weight_2   = 0.1;
-    data.heavy_phase_weight_2 = 0.2;
-
-    data.type = DiracDeltaFunctionApproximationType::reciprocal_times_heaviside_phase_weighted;
-    test(data);
-  }
+  for (const auto type : DiracDeltaFunctionApproximationType::_values())
+    {
+      data.type = type;
+      test(data);
+    }
 }
