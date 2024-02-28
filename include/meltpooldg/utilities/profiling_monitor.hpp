4282c7ce0f3be7b879ef7bfe2700e47697c7d579
Nils Much <nils.much@tum.de>
2024-02-28 13:08:07 +0100

    Compile profiling monitor

--- include/meltpooldg/utilities/profiling_monitor.hpp
+++ include/meltpooldg/utilities/profiling_monitor.hpp
@@ -15,7 +15,6 @@
 #include <meltpooldg/utilities/time_iterator.hpp>
 
 #include <chrono>
-#include <iostream>
 
 namespace MeltPoolDG::Profiling
 {
@@ -23,43 +22,15 @@ namespace MeltPoolDG::Profiling
   class ProfilingMonitor
   {
   public:
-    ProfilingMonitor(const ProfilingData<number> &data, const TimeIterator<number> &time)
-      : data(data)
-      , time(time)
-      , real_time_start(std::chrono::system_clock::now())
-    {
-      last_written_time = compute_current_time();
-    }
+    ProfilingMonitor(const ProfilingData<number> &data, const TimeIterator<number> &time);
 
     bool
-    now() const
-    {
-      if (!data.enable)
-        return false;
-
-      const number current_time = compute_current_time();
-
-      const bool do_output = (current_time - last_written_time) >= data.write_time_step_size;
-
-      if (do_output)
-        last_written_time = current_time;
-
-      return do_output;
-    }
+    now() const;
 
     void
     print(const ConditionalOStream &pcout,
           const TimerOutput        &timer,
-          const MPI_Comm           &mpi_communicator) const
-    {
-      timer.print_wall_time_statistics(mpi_communicator);
-      pcout << std::endl;
-      IterationMonitor::print(pcout);
-      pcout << std::endl;
-      DoFMonitor::print(pcout);
-      pcout << std::endl;
-      CellMonitor::print(pcout);
-    }
+          const MPI_Comm           &mpi_communicator) const;
 
   private:
     const ProfilingData<number>                       &data;
@@ -68,15 +39,6 @@ namespace MeltPoolDG::Profiling
     std::chrono::time_point<std::chrono::system_clock> real_time_start;
 
     number
-    compute_current_time() const
-    {
-      // note: we use nanoseconds to increase the precision of the real time in seconds
-      return (data.time_type == TimeType::real ?
-                std::chrono::duration_cast<std::chrono::nanoseconds>(
-                  std::chrono::system_clock::now() - real_time_start)
-                    .count() /
-                  1e9 :
-                time.get_current_time());
-    }
+    compute_current_time() const;
   };
 } // namespace MeltPoolDG::Profiling
