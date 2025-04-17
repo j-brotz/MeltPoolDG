4b224df22994ff602a78adbcd4a6475884728a41
Magdalena Schreter <magdalena.schreter@tum.de>
2025-04-17 21:50:33 +0200

    fix

--- simulations/melt_front_propagation/melt_front_propagation.hpp
+++ simulations/melt_front_propagation/melt_front_propagation.hpp
@@ -178,68 +178,62 @@ namespace MeltPoolDG::Simulation::MeltFrontPropagation
           this->triangulation =
             std::make_shared<parallel::distributed::Triangulation<2>>(this->mpi_communicator);
 
-          if (this->parameters.base.application_name == "heat_transfer" && !do_two_phase)
-            {
-              std::vector<unsigned> refinements(2, 1);
-              refinements[0] = 3;
-              // create mesh
-              const Point<2> left(0, -z_max);
-              const Point<2> right(x_max, 0);
-              GridGenerator::subdivided_hyper_rectangle(*this->triangulation,
-                                                        refinements,
-                                                        left,
-                                                        right);
-              this->triangulation->refine_global(this->parameters.base.global_refinements);
-            }
-          else
-            {
-              std::vector<unsigned> refinements(2, 1);
-              refinements[0] = 3;
-              refinements[1] = 2;
-              // create mesh
-              const Point<2> left(0, -z_max);
-              const Point<2> right(x_max, z_max);
-              GridGenerator::subdivided_hyper_rectangle(*this->triangulation,
-                                                        refinements,
-                                                        left,
-                                                        right);
-              this->triangulation->refine_global(this->parameters.base.global_refinements);
-            }
+          // if (this->parameters.base.application_name == "heat_transfer" && !do_two_phase)
+          //{
+          // std::vector<unsigned> refinements(2, 1);
+          // refinements[0] = 3;
+          //// create mesh
+          // const Point<2> left(0, -z_max);
+          // const Point<2> right(x_max, 0);
+          // GridGenerator::subdivided_hyper_rectangle(*this->triangulation,
+          // refinements,
+          // left,
+          // right);
+          // this->triangulation->refine_global(this->parameters.base.global_refinements);
+          //}
+          // else
+          //{
+          std::vector<unsigned> refinements(2, 1);
+          refinements[0] = 3;
+          refinements[1] = 2;
+          // create mesh
+          const Point<2> left(0, -z_max);
+          const Point<2> right(x_max, z_max);
+          GridGenerator::subdivided_hyper_rectangle(*this->triangulation, refinements, left, right);
+          this->triangulation->refine_global(this->parameters.base.global_refinements);
+          //}
         }
       else if constexpr (dim == 3)
         {
           this->triangulation =
             std::make_shared<parallel::distributed::Triangulation<3>>(this->mpi_communicator);
 
-          if (this->parameters.base.application_name == "heat_transfer" && !do_two_phase)
-            {
-              std::vector<unsigned int> refinements(3, 1);
-              refinements[0] = 3;
-              refinements[1] = 3;
-              // create mesh
-              const Point<3> left(0, -y_max, -z_max);
-              const Point<3> right(x_max, y_max, z_max);
-              GridGenerator::subdivided_hyper_rectangle(*this->triangulation,
-                                                        refinements,
-                                                        left,
-                                                        right);
-              this->triangulation->refine_global(this->parameters.base.global_refinements);
-            }
-          else
-            {
-              std::vector<unsigned int> refinements(3, 1);
-              refinements[0] = 3;
-              refinements[1] = 3;
-              refinements[2] = 2;
-              // create mesh
-              const Point<3> left(0, 0, z_max);
-              const Point<3> right(x_max, y_max, -z_max);
-              GridGenerator::subdivided_hyper_rectangle(*this->triangulation,
-                                                        refinements,
-                                                        left,
-                                                        right);
-              this->triangulation->refine_global(this->parameters.base.global_refinements);
-            }
+          // if (this->parameters.base.application_name == "heat_transfer" && !do_two_phase)
+          //{
+          std::vector<unsigned int> refinements(3, 1);
+          refinements[0] = 3;
+          refinements[1] = 3;
+          // create mesh
+          const Point<3> left(0, -y_max, -z_max);
+          const Point<3> right(x_max, y_max, z_max);
+          GridGenerator::subdivided_hyper_rectangle(*this->triangulation, refinements, left, right);
+          this->triangulation->refine_global(this->parameters.base.global_refinements);
+          //}
+          // else
+          //{
+          // std::vector<unsigned int> refinements(3, 1);
+          // refinements[0] = 3;
+          // refinements[1] = 3;
+          // refinements[2] = 2;
+          //// create mesh
+          // const Point<3> left(0, 0, z_max);
+          // const Point<3> right(x_max, y_max, -z_max);
+          // GridGenerator::subdivided_hyper_rectangle(*this->triangulation,
+          // refinements,
+          // left,
+          // right);
+          // this->triangulation->refine_global(this->parameters.base.global_refinements);
+          //}
         }
       else
         AssertThrow(false, ExcMessage("Impossible dimension! Abort ..."));
@@ -248,84 +242,84 @@ namespace MeltPoolDG::Simulation::MeltFrontPropagation
     void
     set_boundary_conditions() final
     {
-      if (this->parameters.base.application_name == "heat_transfer")
-        {
-          const types::boundary_id left_bc = 10;
-          for (const auto &cell : this->triangulation->cell_iterators())
+      // if (this->parameters.base.application_name == "heat_transfer")
+      //{
+      // const types::boundary_id left_bc = 10;
+      // for (const auto &cell : this->triangulation->cell_iterators())
+      //{
+      // for (auto &face : cell->face_iterators())
+      // if (face->at_boundary())
+      //{
+      // if (face->center()[0] == x_max)
+      // face->set_boundary_id(left_bc);
+      //}
+      //}
+      // this->attach_boundary_condition(
+      //{left_bc, std::make_shared<dealii::Functions::ConstantFunction<dim>>(T_0)},
+      //"dirichlet",
+      //"heat_transfer");
+      //}
+      // else if (this->parameters.base.application_name == "melt_pool")
+      //{
+      const types::boundary_id                  lower_bc = 1;
+      const types::boundary_id                  upper_bc = 2;
+      const types::boundary_id                  left_bc  = 3;
+      const types::boundary_id                  right_bc = 4;
+      [[maybe_unused]] const types::boundary_id front_bc = 5;
+      [[maybe_unused]] const types::boundary_id back_bc  = 6;
+      for (const auto &cell : this->triangulation->cell_iterators())
+        for (auto &face : cell->face_iterators())
+          if (face->at_boundary())
             {
-              for (auto &face : cell->face_iterators())
-                if (face->at_boundary())
-                  {
-                    if (face->center()[0] == x_max)
-                      face->set_boundary_id(left_bc);
-                  }
-            }
-          this->attach_boundary_condition(
-            {left_bc, std::make_shared<dealii::Functions::ConstantFunction<dim>>(T_0)},
-            "dirichlet",
-            "heat_transfer");
-        }
-      else if (this->parameters.base.application_name == "melt_pool")
-        {
-          const types::boundary_id                  lower_bc = 1;
-          const types::boundary_id                  upper_bc = 2;
-          const types::boundary_id                  left_bc  = 3;
-          const types::boundary_id                  right_bc = 4;
-          [[maybe_unused]] const types::boundary_id front_bc = 5;
-          [[maybe_unused]] const types::boundary_id back_bc  = 6;
-          for (const auto &cell : this->triangulation->cell_iterators())
-            for (auto &face : cell->face_iterators())
-              if (face->at_boundary())
+              if constexpr (dim == 1)
                 {
-                  if constexpr (dim == 1)
+                  if (face->center()[0] == z_max)
+                    face->set_boundary_id(lower_bc);
+                  else if (face->center()[0] == z_min)
+                    face->set_boundary_id(upper_bc);
+                }
+              else
+                {
+                  if (face->center()[0] == x_max)
+                    face->set_boundary_id(right_bc);
+                  else if (face->center()[0] == x_min)
+                    face->set_boundary_id(left_bc);
+
+                  if constexpr (dim == 2)
                     {
-                      if (face->center()[0] == z_max)
+                      if (face->center()[1] == -z_max)
                         face->set_boundary_id(lower_bc);
-                      else if (face->center()[0] == z_min)
+                      if (face->center()[1] == z_max)
                         face->set_boundary_id(upper_bc);
                     }
-                  else
+                  if constexpr (dim == 3)
                     {
-                      if (face->center()[0] == x_max)
-                        face->set_boundary_id(right_bc);
-                      else if (face->center()[0] == x_min)
-                        face->set_boundary_id(left_bc);
-
-                      if constexpr (dim == 2)
-                        {
-                          if (face->center()[1] == -z_max)
-                            face->set_boundary_id(lower_bc);
-                          if (face->center()[1] == z_max)
-                            face->set_boundary_id(upper_bc);
-                        }
-                      if constexpr (dim == 3)
-                        {
-                          if (face->center()[1] == -y_max)
-                            face->set_boundary_id(lower_bc);
-                          if (face->center()[1] == y_max)
-                            face->set_boundary_id(upper_bc);
-                          if (face->center()[2] == -z_max)
-                            face->set_boundary_id(front_bc);
-                          if (face->center()[2] == z_max)
-                            face->set_boundary_id(back_bc);
-                        }
+                      if (face->center()[1] == -y_max)
+                        face->set_boundary_id(lower_bc);
+                      if (face->center()[1] == y_max)
+                        face->set_boundary_id(upper_bc);
+                      if (face->center()[2] == -z_max)
+                        face->set_boundary_id(front_bc);
+                      if (face->center()[2] == z_max)
+                        face->set_boundary_id(back_bc);
                     }
                 }
-          this->attach_boundary_condition(left_bc, "no_slip", "navier_stokes_u");
-          this->attach_boundary_condition(right_bc, "no_slip", "navier_stokes_u");
-          this->attach_boundary_condition(lower_bc, "no_slip", "navier_stokes_u");
-          this->attach_boundary_condition(upper_bc, "symmetry", "navier_stokes_u");
-          if constexpr (dim == 3)
-            {
-              this->attach_boundary_condition(front_bc, "no_slip", "navier_stokes_u");
-              this->attach_boundary_condition(back_bc, "no_slip", "navier_stokes_u");
             }
-          this->attach_boundary_condition(lower_bc, "fix_pressure_constant", "navier_stokes_p");
-          this->attach_boundary_condition(
-            {lower_bc, std::make_shared<dealii::Functions::ConstantFunction<dim>>(T_0)},
-            "dirichlet",
-            "heat_transfer");
+      this->attach_boundary_condition(left_bc, "no_slip", "navier_stokes_u");
+      this->attach_boundary_condition(right_bc, "no_slip", "navier_stokes_u");
+      this->attach_boundary_condition(lower_bc, "no_slip", "navier_stokes_u");
+      this->attach_boundary_condition(upper_bc, "symmetry", "navier_stokes_u");
+      if constexpr (dim == 3)
+        {
+          this->attach_boundary_condition(front_bc, "no_slip", "navier_stokes_u");
+          this->attach_boundary_condition(back_bc, "no_slip", "navier_stokes_u");
         }
+      this->attach_boundary_condition(lower_bc, "fix_pressure_constant", "navier_stokes_p");
+      this->attach_boundary_condition(
+        {lower_bc, std::make_shared<dealii::Functions::ConstantFunction<dim>>(T_0)},
+        "dirichlet",
+        "heat_transfer");
+      //}
     }
 
     void
@@ -333,23 +327,20 @@ namespace MeltPoolDG::Simulation::MeltFrontPropagation
     {
       this->attach_initial_condition(
         std::make_shared<dealii::Functions::ConstantFunction<dim>>(T_0), "heat_transfer");
-      if (this->parameters.base.application_name == "heat_transfer" && do_two_phase)
-        this->attach_initial_condition(std::make_shared<InitialLevelSetHeaviside<dim>>(0.0,
-                                                                                       z_max / 5),
-                                       "prescribed_heaviside");
-      if (this->parameters.base.application_name == "melt_pool")
-        {
-          const double eps =
-            this->parameters.ls.reinit.compute_interface_thickness_parameter_epsilon(
-              GridTools::minimal_cell_diameter(*this->triangulation) /
-              this->parameters.ls.get_n_subdivisions() / std::sqrt(dim));
-          this->attach_initial_condition(std::make_shared<InitialLevelSet<dim>>(0.0, eps),
-                                         "level_set");
-          this->attach_initial_condition(std::shared_ptr<dealii::Function<dim>>(
-                                           std::make_shared<dealii::Functions::ZeroFunction<dim>>(
-                                             dim)),
-                                         "navier_stokes_u");
-        }
+      // if (this->parameters.base.application_name == "heat_transfer" && do_two_phase)
+      // this->attach_initial_condition(std::make_shared<InitialLevelSetHeaviside<dim>>(0.0,
+      // z_max / 5),
+      //"prescribed_heaviside");
+      // if (this->parameters.base.application_name == "melt_pool")
+      //{
+      const double eps = this->parameters.ls.reinit.compute_interface_thickness_parameter_epsilon(
+        GridTools::minimal_cell_diameter(*this->triangulation) /
+        this->parameters.ls.get_n_subdivisions() / std::sqrt(dim));
+      this->attach_initial_condition(std::make_shared<InitialLevelSet<dim>>(0.0, eps), "level_set");
+      this->attach_initial_condition(std::shared_ptr<dealii::Function<dim>>(
+                                       std::make_shared<dealii::Functions::ZeroFunction<dim>>(dim)),
+                                     "navier_stokes_u");
+      //}
     }
   };
 } // namespace MeltPoolDG::Simulation::MeltFrontPropagation
