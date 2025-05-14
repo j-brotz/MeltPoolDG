9d0bd24e356d894850cf2e1cc32efbeb94d0afb3
Julian Brotz <julian.brotz@tum.de>
2025-05-14 15:24:08 +0200

    MPI benchmarks with more than one process

--- benchmarks/README.md
+++ benchmarks/README.md
@@ -1,3 +1,82 @@
-# On the Usage of the Benchmarking Framework
+# Using the Benchmarking Framework
 
-work in progress
+The benchmarks/ directory contains everything needed to set up and run custom benchmarks to evaluate and improve
+specific parts of the MeltPoolDG library. This framework is based on Google Benchmark, and standard usage is consistent
+with the official Google Benchmark user guide.
+
+## Enabling Benchmarking
+
+To use the benchmarking framework, compile MeltPoolDG with the MPDG_ENABLE_BENCHMARKING flag enabled. You don’t need to
+manually install any additional dependencies — CMake will handle everything for you. Setting this flag instructs CMake
+to:
+
+- Install the required benchmark dependencies
+- Generate build targets for all benchmarks found in subfolders of benchmarks/
+
+## Adding New Benchmarks
+
+If you create a new subfolder inside benchmarks/, make sure it includes a CMakeLists.txt that calls the function:
+
+```
+mpdg_auto_setup_benchmarks()
+```
+
+This function automatically generates an executable for each .cpp file in that folder. In most cases, this is all you
+need. If you have more specific setup requirements, you can customize the CMakeLists.txt as needed.
+
+For general instructions on writing benchmark executables, refer to the Google Benchmark User Guide.
+
+## Benchmarking MPI Code
+
+### Using a Single MPI Process
+
+When benchmarking MPI-based code with one process, only one change is required compared to standard Google Benchmark
+usage:
+
+- Include `benchmark_util.h`
+- Replace `BENCHMARK_MAIN()` with `MPDG_BENCHMARK_MPI_MAIN`
+
+This ensures that the MPI environment is properly initialized before any MPI-related functionality is used.
+
+### Using Multiple MPI Processes
+
+Benchmarking code with multiple MPI processes is a bit more involved—and more restricted — than single-process
+benchmarking. However, from the user perspective, only minimal changes are needed in the benchmark executable.
+
+Instead of the typical Google Benchmark loop:
+
+```cpp
+for (auto _ : state)
+    // code to be benchmarked
+```
+
+you must use a helper function provided in `benchmark_util.hpp`:
+
+```cpp
+void mpi_benchmark_loop(benchmark::State &state, const std::function<void()> &benchmark_function)
+```
+
+where `state` is the `Benchmark::State` object used by the current benchmark and `benchmark_function` is the code
+you want to benchmark wrapped in a `std::function<void()>`.
+
+#### Why This Change Is Necessary
+
+In the default Google Benchmark setup, each process independently determines how many iterations to run based on its
+local timing. In an MPI environment, this can lead to problems—some processes may exit the loop earlier than others,
+potentially causing deadlocks if remaining processes are still running collective operations.
+
+The mpi_benchmark_loop() function avoids this by synchronizing iteration counts across all processes using an
+MPI_Allreduce. It ensures that all ranks run the same number of iterations, making the benchmark results statistically
+stable and deadlock-free.
+
+#### Current Limitations of Multiple Process MPI Benchmarks
+
+While the multi-process MPI benchmarking system in MeltPoolDG works reliably, there are a few limitations you should be
+aware of:
+
+- Reduced Output: Compared to single-process benchmarks, the console output is minimal.
+    - Only the wall-clock time, number of iterations, and benchmark name are printed.
+    - Any custom counters are also printed — but only those from rank 0.
+
+However, the key advantage is that the benchmark reports individual wall-clock times for each MPI process. This provides
+valuable insights into effects like load imbalance.
--- benchmarks/flow/benchmark_compressible_flow_operator.cpp
+++ benchmarks/flow/benchmark_compressible_flow_operator.cpp
@@ -1,6 +1,9 @@
 /**
- * @brief Benchmark for the explicit operator of the compressible flow solver. The benchmarks in
- * this file benchmarks the apply_operator() method for viscid and in-viscid flows.
+ * @brief Benchmark for the explicit, implicit, and IMEX operators of the compressible flow solver.
+ *
+ * This benchmark targets the apply_operator() method for both viscous and inviscid flows. The test
+ * domain is a cube with an equal number of cells in each direction, representing a channel flow
+ * scenario with inflow, outflow, and no-slip boundary conditions.
  */
 
 #include <deal.II/distributed/tria.h>
@@ -10,6 +13,8 @@
 #include <deal.II/grid/grid_generator.h>
 #include <deal.II/grid/tria.h>
 
+#include <deal.II/matrix_free/operators.h>
+
 #include <meltpooldg/core/scratch_data.hpp>
 #include <meltpooldg/flow/compressible_flow_data.hpp>
 #include <meltpooldg/flow/compressible_flow_utils.hpp>
@@ -17,6 +22,7 @@
 #include <meltpooldg/flow/dg_compressible_flow_operator_implicit.hpp>
 #include <meltpooldg/flow/dg_compressible_flow_operator_implicit_explicit.hpp>
 #include <meltpooldg/utilities/fe_util.hpp>
+#include <meltpooldg/utilities/vector_tools.templates.hpp>
 
 #include <benchmark/benchmark.h>
 #include <benchmark_util.hpp>
@@ -26,9 +32,14 @@
 
 namespace
 {
+  // Dimension used in the computations.
   constexpr int dim = 3;
-  using number      = double;
+  // Data type used throughout for numerical values.
+  using number = double;
 
+  /**
+   * @brief Enumeration of available flow operator types.
+   */
   enum OperatorType
   {
     Explicit,
@@ -37,7 +48,8 @@ namespace
   };
 
   /**
-   * Function used for boundary and initial conditions.
+   * @brief Helper function class to define boundary and initial conditions for the benchmarks in
+   * this file.
    */
   template <unsigned int dim, typename number>
   class InflowFlowField : public dealii::Function<dim, number>
@@ -61,14 +73,34 @@ namespace
     }
   }; // namespace
 
+  /**
+   * A fixture class providing everything required to set up the (non-cut) compressible flow
+   * operator for explicit, implicit and imex time integration.
+   */
   template <typename is_viscous_bool>
   class CompressibleFlowOperatorFixture : public benchmark::Fixture
   {
     static const bool is_viscous = is_viscous_bool::value;
 
   public:
+    /**
+     * @brief Holds all operator-specific objects and functions.
+     *
+     * This struct is used to simplify data management. By grouping all related resources under
+     * RAII, they are automatically released when the struct is destroyed. This ensures safe and
+     * error-free cleanup in the TearDown function, avoiding potential memory issues from forgotten
+     * deallocations.
+     */
     struct Data
     {
+      /**
+       * @brief Constructor that sets up all necessary components required for finally setting up
+       * the flow oeprator. Initializes the scratch data, including triangulation, DoF handler, and
+       * other required components. Also sets the initial and boundary conditions.
+       *
+       * @param state Google Benchmark state object used to determine the number of cells in each
+       * spatial direction of the cubic domain.
+       */
       explicit Data(const benchmark::State &state)
         : triangulation(MPI_COMM_WORLD)
         , scratch_data(MPI_COMM_WORLD, 0, true)
@@ -102,6 +134,12 @@ namespace
         imex_flow_operator;
 
     private:
+      /**
+       * @brief Initialize scratch data and flow-specific scratch data.
+       *
+       * Sets up the main scratch data object (including mapping, quadrature,
+       * DoF handler, and constraints) and constructs the flow scratch data.
+       */
       void
       setup_scratch_data()
       {
@@ -124,6 +162,9 @@ namespace
         flow_scratch_data->reinit(2);
       }
 
+      /**
+       * @brief Set simulation parameters explicitly, as defaults are not suitable.
+       */
       void
       set_simulation_parameters()
       {
@@ -131,12 +172,16 @@ namespace
         fe_data.degree = 2;
       }
 
+      /**
+       * @brief Set initial and boundary conditions. Applies inflow and outflow boundary conditions
+       * based on the predefined inflow field. All other boundaries use no-slip conditions.
+       */
       void
       set_initial_and_boundary_conditions()
       {
         // set the initial condition
-        InflowFlowField<dim, number>                   function(0.);
-        dealii::FECellIntegrator<dim, dim + 2, number> phi(
+        InflowFlowField<dim, number>                       function(0.);
+        MeltPoolDG::FECellIntegrator<dim, dim + 2, number> phi(
           flow_scratch_data->scratch_data.get_matrix_free(),
           flow_scratch_data->dof_idx,
           flow_scratch_data->quad_idx);
@@ -181,6 +226,13 @@ namespace
       }
     };
 
+    /**
+     * @brief Initializes the compressible flow operator based on the specified type.
+     *
+     * @param type Enum specifying the desired flow operator type.
+     *
+     * @throws dealii::ExcMessage if an unsupported operator type is passed.
+     */
     void
     setup_operator(const OperatorType type)
     {
@@ -210,55 +262,67 @@ namespace
         }
     }
 
-    // prevent compiler hiding warning (-Woverloaded-virtual)
+    // Prevent compiler warnings about hiding overloaded virtual functions (-Woverloaded-virtual).
     using benchmark::Fixture::SetUp;
     using benchmark::Fixture::TearDown;
 
+
+    /**
+     * @brief Fixture setup function called before each benchmark run. Initializes the
+     * benchmark-specific data structure using the benchmark state.
+     *
+     * @param state Reference to the current benchmark state.
+     */
     void
     SetUp(benchmark::State &state) override
     {
       data = std::make_unique<Data>(state);
     }
 
+
+    /**
+     * @brief Fixture teardown function called after each benchmark run. Cleans up and deallocates
+     * the benchmark-specific data, i.e., the data object struct.
+     */
     void
     TearDown(benchmark::State &) override
     {
       data.reset();
     }
 
+    // Holds all data for the compressible flow operator.
     std::unique_ptr<Data> data;
   };
 
-  BENCHMARK_TEMPLATE_METHOD_F(CompressibleFlowOperatorFixture,
-                              ExplicitOperator)(benchmark::State &state)
+  BENCHMARK_TEMPLATE_METHOD_F(CompressibleFlowOperatorFixture, ExplicitOperator)
+  (benchmark::State &state)
   {
     this->setup_operator(OperatorType::Explicit);
-    for (auto _ : state)
-      {
-        this->data->explicit_flow_operator->apply_operator(
-          0.,
-          this->data->flow_scratch_data->solution_history.get_current_solution(),
-          this->data->flow_scratch_data->solution_history.get_current_solution(),
-          std::function<void(unsigned int, unsigned int)>());
-      }
-    state.counters["DoFs"] = this->data->dof_handler.n_dofs();
+    MeltPoolDG::mpi_benchmark_loop(state, [&]() {
+      this->data->explicit_flow_operator->apply_operator(
+        0.,
+        this->data->flow_scratch_data->solution_history.get_current_solution(),
+        this->data->flow_scratch_data->solution_history.get_recent_old_solution(),
+        std::function<void(unsigned int, unsigned int)>());
+      state.counters["DoFs"] = this->data->dof_handler.n_dofs();
+    });
   }
 
   BENCHMARK_TEMPLATE_INSTANTIATE_F(CompressibleFlowOperatorFixture,
                                    ExplicitOperator,
                                    std::integral_constant<bool, true>)
-    ->DenseRange(5, 10, 5)
+    ->DenseRange(10, 20, 10)
     ->Name("explicit operator, viscid");
 
   BENCHMARK_TEMPLATE_INSTANTIATE_F(CompressibleFlowOperatorFixture,
                                    ExplicitOperator,
                                    std::integral_constant<bool, false>)
-    ->DenseRange(5, 10, 5)
+    ->DenseRange(10, 20, 10)
     ->Name("explicit operator, in-viscid");
 
 
-  BENCHMARK_TEMPLATE_METHOD_F(CompressibleFlowOperatorFixture,
-                              ImplicitOperatorResidual)(benchmark::State &state)
+  BENCHMARK_TEMPLATE_METHOD_F(CompressibleFlowOperatorFixture, ImplicitOperatorResidual)
+  (benchmark::State &state)
   {
     constexpr number current_time = 0.1;
     constexpr number time_step    = 1e-4;
@@ -268,30 +332,29 @@ namespace
       current_time,
       time_step,
       this->data->flow_scratch_data->solution_history.get_recent_old_solution());
-    for (auto _ : state)
-      {
-        this->data->implicit_flow_operator->compute_residual(
-          0.1,
-          this->data->flow_scratch_data->solution_history.get_solution(1),
-          this->data->flow_scratch_data->solution_history.get_current_solution());
-      }
+    MeltPoolDG::mpi_benchmark_loop(state, [&]() {
+      this->data->implicit_flow_operator->compute_residual(
+        0.1,
+        this->data->flow_scratch_data->solution_history.get_solution(1),
+        this->data->flow_scratch_data->solution_history.get_current_solution());
+    });
     state.counters["DoFs"] = this->data->dof_handler.n_dofs();
   }
 
   BENCHMARK_TEMPLATE_INSTANTIATE_F(CompressibleFlowOperatorFixture,
                                    ImplicitOperatorResidual,
                                    std::integral_constant<bool, true>)
-    ->DenseRange(5, 10, 5)
+    ->DenseRange(10, 20, 10)
     ->Name("implicit operator, viscid: residual");
 
   BENCHMARK_TEMPLATE_INSTANTIATE_F(CompressibleFlowOperatorFixture,
                                    ImplicitOperatorResidual,
                                    std::integral_constant<bool, false>)
-    ->DenseRange(5, 10, 5)
+    ->DenseRange(10, 20, 10)
     ->Name("implicit operator, in-viscid: residual");
 
-  BENCHMARK_TEMPLATE_METHOD_F(CompressibleFlowOperatorFixture,
-                              ImplicitOperatorJacobian)(benchmark::State &state)
+  BENCHMARK_TEMPLATE_METHOD_F(CompressibleFlowOperatorFixture, ImplicitOperatorJacobian)
+  (benchmark::State &state)
   {
     constexpr number current_time = 0.1;
     constexpr number time_step    = 1e-4;
@@ -301,29 +364,28 @@ namespace
       current_time,
       time_step,
       this->data->flow_scratch_data->solution_history.get_recent_old_solution());
-    for (auto _ : state)
-      {
-        this->data->implicit_flow_operator->apply_jacobian(
-          this->data->flow_scratch_data->solution_history.get_current_solution(),
-          this->data->flow_scratch_data->solution_history.get_recent_old_solution());
-      }
+    MeltPoolDG::mpi_benchmark_loop(state, [&]() {
+      this->data->implicit_flow_operator->apply_jacobian(
+        this->data->flow_scratch_data->solution_history.get_current_solution(),
+        this->data->flow_scratch_data->solution_history.get_recent_old_solution());
+    });
     state.counters["DoFs"] = this->data->dof_handler.n_dofs();
   }
 
   BENCHMARK_TEMPLATE_INSTANTIATE_F(CompressibleFlowOperatorFixture,
                                    ImplicitOperatorJacobian,
                                    std::integral_constant<bool, true>)
-    ->DenseRange(5, 10, 5)
+    ->DenseRange(10, 20, 10)
     ->Name("implicit operator, viscid: jacobian");
 
   BENCHMARK_TEMPLATE_INSTANTIATE_F(CompressibleFlowOperatorFixture,
                                    ImplicitOperatorJacobian,
                                    std::integral_constant<bool, false>)
-    ->DenseRange(5, 10, 5)
+    ->DenseRange(10, 20, 10)
     ->Name("implicit operator, in-viscid: jacobian");
 
-  BENCHMARK_TEMPLATE_METHOD_F(CompressibleFlowOperatorFixture,
-                              ImExOperatorResidual)(benchmark::State &state)
+  BENCHMARK_TEMPLATE_METHOD_F(CompressibleFlowOperatorFixture, ImExOperatorResidual)
+  (benchmark::State &state)
   {
     constexpr number current_time = 0.1;
     constexpr number time_step    = 1e-4;
@@ -333,25 +395,24 @@ namespace
       current_time,
       time_step,
       this->data->flow_scratch_data->solution_history.get_recent_old_solution());
-    for (auto _ : state)
-      {
-        this->data->imex_flow_operator->compute_residual(
-          0.1,
-          this->data->flow_scratch_data->solution_history.get_solution(1),
-          this->data->flow_scratch_data->solution_history.get_current_solution());
-      }
+    MeltPoolDG::mpi_benchmark_loop(state, [&]() {
+      this->data->imex_flow_operator->compute_residual(
+        0.1,
+        this->data->flow_scratch_data->solution_history.get_solution(1),
+        this->data->flow_scratch_data->solution_history.get_current_solution());
+    });
     state.counters["DoFs"] = this->data->dof_handler.n_dofs();
   }
 
   BENCHMARK_TEMPLATE_INSTANTIATE_F(CompressibleFlowOperatorFixture,
                                    ImExOperatorResidual,
                                    std::integral_constant<bool, true>)
-    ->DenseRange(5, 10, 5)
+    ->DenseRange(10, 20, 10)
     ->Name("imex operator, viscid: residual");
 
 
-  BENCHMARK_TEMPLATE_METHOD_F(CompressibleFlowOperatorFixture,
-                              ImExOperatorJacobian)(benchmark::State &state)
+  BENCHMARK_TEMPLATE_METHOD_F(CompressibleFlowOperatorFixture, ImExOperatorJacobian)
+  (benchmark::State &state)
   {
     constexpr number current_time = 0.1;
     constexpr number time_step    = 1e-4;
@@ -361,43 +422,41 @@ namespace
       current_time,
       time_step,
       this->data->flow_scratch_data->solution_history.get_recent_old_solution());
-    for (auto _ : state)
-      {
-        this->data->imex_flow_operator->apply_jacobian(
-          this->data->flow_scratch_data->solution_history.get_current_solution(),
-          this->data->flow_scratch_data->solution_history.get_recent_old_solution());
-      }
+    MeltPoolDG::mpi_benchmark_loop(state, [&]() {
+      this->data->imex_flow_operator->apply_jacobian(
+        this->data->flow_scratch_data->solution_history.get_current_solution(),
+        this->data->flow_scratch_data->solution_history.get_recent_old_solution());
+    });
     state.counters["DoFs"] = this->data->dof_handler.n_dofs();
   }
 
   BENCHMARK_TEMPLATE_INSTANTIATE_F(CompressibleFlowOperatorFixture,
                                    ImExOperatorJacobian,
                                    std::integral_constant<bool, true>)
-    ->DenseRange(5, 10, 5)
+    ->DenseRange(10, 20, 10)
     ->Name("imex operator, viscid: jacobian");
 
-  BENCHMARK_TEMPLATE_METHOD_F(CompressibleFlowOperatorFixture,
-                              ImExOperatorExplicitStage)(benchmark::State &state)
+  BENCHMARK_TEMPLATE_METHOD_F(CompressibleFlowOperatorFixture, ImExOperatorExplicitStage)
+  (benchmark::State &state)
   {
     constexpr number current_time = 0.1;
     constexpr number time_step    = 1e-4;
     this->setup_operator(OperatorType::ImEx);
     this->data->flow_scratch_data->solution_history.commit_old_solutions();
-    for (auto _ : state)
-      {
-        this->data->imex_flow_operator->perform_explicit_stage(
-          current_time,
-          time_step,
-          this->data->flow_scratch_data->solution_history.get_current_solution(),
-          this->data->flow_scratch_data->solution_history.get_recent_old_solution());
-      }
+    MeltPoolDG::mpi_benchmark_loop(state, [&]() {
+      this->data->imex_flow_operator->perform_explicit_stage(
+        current_time,
+        time_step,
+        this->data->flow_scratch_data->solution_history.get_current_solution(),
+        this->data->flow_scratch_data->solution_history.get_recent_old_solution());
+    });
     state.counters["DoFs"] = this->data->dof_handler.n_dofs();
   }
 
   BENCHMARK_TEMPLATE_INSTANTIATE_F(CompressibleFlowOperatorFixture,
                                    ImExOperatorExplicitStage,
                                    std::integral_constant<bool, true>)
-    ->DenseRange(5, 10, 5)
+    ->DenseRange(10, 20, 10)
     ->Name("imex operator, viscid: explicit stage");
 } // namespace
 
