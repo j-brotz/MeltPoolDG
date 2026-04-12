9c063a4da592293a7c18f16c921bb6f586d17d90
Julian Brotz <julian.brotz@tum.de>
2026-04-12 14:13:53 +0200

    Add free jet inflow boundary condition

--- include/meltpooldg/compressible_flow/case_utils.hpp
+++ include/meltpooldg/compressible_flow/case_utils.hpp
@@ -1,10 +1,12 @@
 #pragma once
 
 #include <deal.II/base/function.h>
+#include <deal.II/base/parameter_handler.h>
 #include <deal.II/base/point.h>
 
 #include <deal.II/grid/tria.h>
 
+#include <meltpooldg/compressible_flow/boundary_condition_functions.hpp>
 #include <meltpooldg/compressible_flow/boundary_conditions.hpp>
 #include <meltpooldg/compressible_flow/data_types.hpp>
 
@@ -14,140 +16,62 @@
 
 namespace MeltPoolDG::CompressibleFlow
 {
-  /// Enum for the type of ramp up function used for the velocity at an inflow boundary.
-  BETTER_ENUM(RampUpType, char, none, linear, exponential, cosine);
-
   /**
-   * A function class that computes the conservative variable values for the
-   * compressible Navier–Stokes equations based on provided functions for the primitive variables:
-   * density, velocity, specific inner energy and species mass fractions.
-   *
-   * Optionally, a time-dependent ramp-up can be applied to the velocity.
+   * Struct for storing free jet inflow boundary condition information and creating
+   * a corresponding function object for use in simulations. This can be used to provide cases where
+   * via the input file the user is allowed to specify a free jet inflow boundary condition with
+   * user-defined parameters for the free jet inflow.
    */
   template <int dim, typename number>
-  class ConservativeVariablesFunction : public dealii::Function<dim, number>
+  struct InputDefinedFreeJetInflow
   {
-  public:
     /**
-     * Constructor, stores the passed arguments internally.
+     * Creates a FreeJetInflow function object based on the stored parameters for the free jet
+     * inflow which can be used to apply the free jet inflow boundary condition in simulations.
      *
-     * @param initial_time Initial time used to initialize the function.
-     * @param density Function representing the density field.
-     * @param velocity Function representing the velocity field.
-     * @param inner_energy Function representing the specific inner energy field.
-     * @param mass_fractions Function representing the species mass fractions (only used for
-     * multi-component cases).
+     * @param start_time The initial time at which the free jet inflow boundary condition is applied.
+     * @param velocity_ramp_up_duration The duration over which the velocity ramps up.
+     * @param velocity_ramp_up_type The type of ramp up for the velocity.
+     * @param n_species The total number of species in the simulation.
      */
-    ConservativeVariablesFunction(
-      const number                                         initial_time,
-      const std::shared_ptr<dealii::Function<dim, number>> density,
-      const std::shared_ptr<dealii::Function<dim, number>> velocity,
-      const std::shared_ptr<dealii::Function<dim, number>> inner_energy,
-      const std::shared_ptr<dealii::Function<dim, number>> mass_fractions);
-
+    std::shared_ptr<dealii::Function<dim, number>>
+    create_free_jet_inflow_boundary(const number     start_time,
+                                    const number     velocity_ramp_up_duration,
+                                    const RampUpType velocity_ramp_up_type,
+                                    const unsigned   n_species = 1) const;
     /**
-     * Constructor for the multi-component case, where the individual primitive variable functions
-     * are passed as strings and initialized as FunctionParser objects within the constructor.
+     * Adds the parameters for the free jet inflow to a given ParameterHandler object.
      *
-     * @param initial_time Initial time used to initialize the function.
-     * @param density_function_string String representing the function for the density field.
-     * @param velocity_function_string String representing the function for the velocity field.
-     * @param inner_energy_function_string String representing the function for the specific inner energy field.
-     * @param mass_fraction_functions_string String representing the functions for the mass fractions.
-     * The mass fractions should be provided as a semicolon-separated list of functions.
-     * @param n_mass_fractions The total number of mass fractions in the simulation.
-     */
-    ConservativeVariablesFunction(const number       initial_time,
-                                  const std::string &density_function_string,
-                                  const std::string &velocity_function_string,
-                                  const std::string &inner_energy_function_string,
-                                  const std::string &mass_fraction_functions_string,
-                                  const unsigned     n_mass_fractions);
-
-
-    /**
-     * Constructor for single-species flow cases. As above the individual primitive variable
-     * functions are passed as strings and initialized as FunctionParser objects within the
-     * constructor.
-     *
-     * @param initial_time Initial time used to initialize the function.
-     * @param density_function_string String representing the function for the density field.
-     * @param velocity_function_string String representing the function for the velocity field.
-     * @param inner_energy_function_string String representing the function for the specific inner energy field.
-     */
-    ConservativeVariablesFunction(const number       initial_time,
-                                  const std::string &density_function_string,
-                                  const std::string &velocity_function_string,
-                                  const std::string &inner_energy_function_string);
-
-    /**
-     * Sets the velocity ramp-up parameters. If this function is never called or if a ramp-up
-     * duration of zero is specified, no ramp-up is applied and the returned values correspond
-     * directly to the fully developed velocity field.
-     *
-     * @param duration The duration of the ramp-up.
-     * @param type The type of ramp-up function.
+     * @param prm The ParameterHandler object to which the parameters should be added.
      */
     void
-    set_velocity_ramp_up(const number duration, const RampUpType type)
-    {
-      velocity_ramp_up.duration = duration;
-      velocity_ramp_up.type     = type;
-    }
+    add_parameters(dealii::ParameterHandler &prm);
 
-    /**
-     * Set the time for the function.
-     *
-     * @param new_time Time to be set.
-     */
-    void
-    set_time(const number new_time) override;
+    /// Boolean flag to determine whether the free jet inflow boundary condition should be applied
+    /// at the specified boundary.
+    bool set_free_jet_inflow{false};
 
-    /**
-     * @brief Evaluates the conservative variable value for a given component at a specified location.
-     *
-     * This function returns the value of the conservative variable corresponding to the specified
-     * component at the given spatial location @p loc. The value is computed using the density,
-     * velocity, and energy functions provided to the object during construction.
-     *
-     * If a ramp-up duration is defined, the velocity components are scaled according to the
-     * selected ramp-up function depending on the current time.
-     *
-     * If no inflow ramp-up duration is specified (i.e., @p inflow_ramp_up_duration = 0),
-     * the returned value corresponds directly to the fully developed flow field.
-     *
-     * @param loc Coordinates at which the function value is evaluated.
-     * @param component  The index of the conservative variable component to evaluate.
-     *
-     * @return The value of the specified conservative variable component at @p loc.
-     */
-    number
-    value(const dealii::Point<dim, number> &loc, const unsigned int component) const override;
+    /// Jet-hole diameter
+    number jet_hole_diameter;
 
-  private:
-    /// Function describing the density field.
-    const std::shared_ptr<dealii::Function<dim, number>> density;
+    /// Jet-hole density at the inflow boundary
+    number jet_hole_density;
 
-    /// Vectorial function describing the velocity field.
-    const std::shared_ptr<dealii::Function<dim, number>> velocity;
+    /// Jet-hole specific inner energy at the inflow boundary
+    number jet_hole_inner_energy;
 
-    /// Function describing the field of the specific total energy.
-    const std::shared_ptr<dealii::Function<dim, number>> inner_energy;
+    /// Jet-hole mass fractions at the inflow boundary (only relevant for multi-component flow
+    /// cases)
+    std::vector<number> jet_hole_mass_fractions;
 
-    /// Function describing the mass fractions for multi-component flow cases.
-    const std::shared_ptr<dealii::Function<dim, number>> mass_fractions;
+    /// Jet-hole center coordinates
+    dealii::Point<dim, number> jet_hole_center;
 
-    struct RampUpParameters
-    {
-      /// Ramp-up duration
-      number duration{0.};
+    /// Flow jet peak velocity
+    number jet_peak_velocity;
 
-      /// Ramp-up function type
-      RampUpType type{RampUpType::none};
-    };
-
-    /// Ramp-up parameters for the velocity function
-    RampUpParameters velocity_ramp_up;
+    /// The velocity profile of the free jet
+    FreeJetProfile free_jet_profile{FreeJetProfile::constant};
   };
 
 
@@ -195,6 +119,10 @@ namespace MeltPoolDG::CompressibleFlow
 
     /// Velocity ramp-up parameters, only relevant for inflow boundaries
     RampUpParameters inflow_velocity_ramp_up;
+
+    /// Free jet inflow parameters, only relevant if the boundary condition type is set to a free
+    /// jet inflow via the enum `combined_inflow_no_slip_wall`.
+    InputDefinedFreeJetInflow<dim, number> free_jet_inflow;
 
     /**
      * Creates a function object representing the boundary condition based on the data stored
@@ -349,5 +277,4 @@ namespace MeltPoolDG::CompressibleFlow
     /// at indices 0, 1, and 2, respectively.
     std::vector<bool> periodic_boundaries = std::vector<bool>(dim, false);
   };
-
 } // namespace MeltPoolDG::CompressibleFlow
