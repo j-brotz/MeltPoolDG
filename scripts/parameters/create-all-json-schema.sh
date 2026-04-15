4c0a0e194bf1e6a73ad3ce2cc15ffa6955bd555d
Magdalena Schreter <magdalena.schreter@tum.de>
2026-04-15 12:57:06 +0200

    add schema to markdown

--- scripts/parameters/create-all-json-schema.sh
+++ scripts/parameters/create-all-json-schema.sh
@@ -8,6 +8,14 @@ if [[ "${1:-}" == "--verbose" ]]; then
   VERBOSE=true
 fi
 
+# force generation of markdown also if schema files didn't change
+FORCE_MD=false
+
+# Parse optional flag
+if [[ "${1:-}" == "--force_md" ]]; then
+  FORCE_MD=true
+fi
+
 find applications/ -type f -name "detailed_parameters.output" -print0 |
 while IFS= read -r -d '' file; do
   rel_path="${file#applications/}"
@@ -26,7 +34,15 @@ while IFS= read -r -d '' file; do
 
   if [ ! -f "$out" ] || ! cmp -s "$tmp" "$out"; then
     mv "$tmp" "$out"
+    # generate docs only if schema has changed
+    python3 scripts/parameters/schema_to_md.py "$out"
   else
     rm -f "$tmp"
+    # force generation of markdown
+    if $FORCE_MD; then
+        python3 scripts/parameters/schema_to_md.py "$out"
+    fi
   fi
+
+
 done
