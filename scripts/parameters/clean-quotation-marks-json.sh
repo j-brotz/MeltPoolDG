3c77cc652908306cc73937c840361582b70fd574
Magdalena Schreter-Fleischhacker <schreter.magdalena@gmail.com>
2026-05-04 17:53:36 +0200

    update

--- scripts/parameters/clean-quotation-marks-json.sh
+++ scripts/parameters/clean-quotation-marks-json.sh
@@ -3,17 +3,15 @@ set -e
 
 status=0
 
-# Find all relevant json files
-find applications -type f -name "*.json" ! -name "*parameters.schema.json" | while read -r json_file; do
-
+while IFS= read -r json_file; do
   tmp="$(mktemp)"
+
   sed -E '
     s/^([[:space:]]*"[^"]+"[[:space:]]*:[[:space:]]*)"[[:space:]]*([+-]?[0-9]+)[.][[:space:]]*"([[:space:]]*,?)[[:space:]]*$/\1\2\3/
     s/^([[:space:]]*"[^"]+"[[:space:]]*:[[:space:]]*)"[[:space:]]*([+-]?[0-9]+[.][0-9]+([eE][+-]?[0-9]+)?|[+-]?[0-9]+([eE][+-]?[0-9]+)?)[[:space:]]*"([[:space:]]*,?)[[:space:]]*$/\1\2\5/
     s/^([[:space:]]*"[^"]+"[[:space:]]*:[[:space:]]*)"[[:space:]]*(true|false)[[:space:]]*"([[:space:]]*,?)[[:space:]]*$/\1\2\3/
   ' "$json_file" > "$tmp"
 
-  # Ensure final newline
   if [ -s "$tmp" ]; then
     last_byte="$(tail -c 1 "$tmp" | od -An -t x1 | tr -d ' \n')"
     if [ "$last_byte" != "0a" ]; then
@@ -27,7 +25,8 @@ find applications -type f -name "*.json" ! -name "*parameters.schema.json" | whi
   else
     rm "$tmp"
   fi
-
-done
+done < <(
+  find applications -type f -name "*.json" ! -name "*parameters.schema.json"
+)
 
 exit "$status"
