3704c539606289443ee8684628116a8fbfbd079d
Anna Frolova <go56cuz2@cm4login2.cos.lrz.de>
2026-05-20 17:10:39 +0200

    style: update parameter names for the pressure aware model

--- source/phase_change/evaporation_data.cpp
+++ source/phase_change/evaporation_data.cpp
@@ -50,7 +50,7 @@ namespace MeltPoolDG::Evaporation
           "Km",
           pressure_aware.Km,
           "Fitting parameters for the evaporative mass flux function with pressure-aware boundary conditions.");
-        prm.add_parameter("ambient_gas_pressure",
+        prm.add_parameter("ambient gas pressure",
                           pressure_aware.ambient_gas_pressure,
                           "Ambient gas pressure for the pressure-aware model.");
       }
