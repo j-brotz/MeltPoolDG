7b08641556e3847c5c972f8a1a707834d1c88d70
Julian Brotz <julian.brotz@tum.de>
2026-07-09 18:21:21 +0200

    refactor(compressible_flow): Simplifies n species views type aliases

--- include/meltpooldg/compressible_flow/state_views_n_species.hpp
+++ include/meltpooldg/compressible_flow/state_views_n_species.hpp
@@ -14,36 +14,15 @@
  */
 namespace MeltPoolDG::CompressibleFlow
 {
-  template <int dim, int n_species, IsConservedStateCompatible<dim> StateType>
-  struct ConcreteDofValueViewImpl
-  {
-    using type = MultiSpeciesDofValueView<dim, n_species, StateType>;
-  };
-
-  template <int dim, IsConservedStateCompatible<dim> StateType>
-  struct ConcreteDofValueViewImpl<dim, 1, StateType>
-  {
-    using type = DofValueView<dim, StateType>;
-  };
-
   /**
    * Type alias for the DofValueView for a specific number of species. This alias resolves to the
    * appropriate state view type depending on the number of species in the simulation.
    */
   template <int dim, int n_species, IsConservedStateCompatible<dim> StateType>
-  using NSpeciesDofValueView = typename ConcreteDofValueViewImpl<dim, n_species, StateType>::type;
-
-  template <int dim, int n_species, typename number, IsConservedStateCompatible<dim> StateType>
-  struct ConcreteDofStateViewImpl
-  {
-    using type = MultiSpeciesDofStateView<dim, n_species, number, StateType>;
-  };
-
-  template <int dim, typename number, IsConservedStateCompatible<dim> StateType>
-  struct ConcreteDofStateViewImpl<dim, 1, number, StateType>
-  {
-    using type = DofStateView<dim, number, StateType>;
-  };
+  using NSpeciesDofValueView =
+    std::conditional_t<n_species == 1,
+                       DofValueView<dim, StateType>,
+                       MultiSpeciesDofValueView<dim, n_species, StateType>>;
 
   /**
    * Type alias for the DofStateView for a specific number of species. This alias resolves to the
@@ -51,26 +30,9 @@ namespace MeltPoolDG::CompressibleFlow
    */
   template <int dim, int n_species, typename number, IsConservedStateCompatible<dim> StateType>
   using NSpeciesDofStateView =
-    typename ConcreteDofStateViewImpl<dim, n_species, number, StateType>::type;
-
-  template <int dim,
-            int n_species,
-            typename number,
-            IsConservedStateCompatible<dim>    Value,
-            IsConservedGradientCompatible<dim> Gradient>
-  struct ConcreteDofValueAndGradientStateViewImpl
-  {
-    using type = MultiSpeciesDofValueAndGradientStateView<dim, n_species, number, Value, Gradient>;
-  };
-
-  template <int dim,
-            typename number,
-            IsConservedStateCompatible<dim>    Value,
-            IsConservedGradientCompatible<dim> Gradient>
-  struct ConcreteDofValueAndGradientStateViewImpl<dim, 1, number, Value, Gradient>
-  {
-    using type = DofValueAndGradientStateView<dim, number, Value, Gradient>;
-  };
+    std::conditional_t<n_species == 1,
+                       DofStateView<dim, number, StateType>,
+                       MultiSpeciesDofStateView<dim, n_species, number, StateType>>;
 
   /**
    * Type alias for the DofValueAndGradientStateView for a specific number of species. This alias
@@ -82,27 +44,18 @@ namespace MeltPoolDG::CompressibleFlow
             typename number,
             IsConservedStateCompatible<dim>    Value,
             IsConservedGradientCompatible<dim> Gradient>
-  using NSpeciesDofValueAndGradientStateView =
-    typename ConcreteDofValueAndGradientStateViewImpl<dim, n_species, number, Value, Gradient>::
-      type;
-
-  template <int dim, int n_species, typename FluxType>
-  struct ConcreteFluxViewImpl
-  {
-    using type = MultiSpeciesFluxView<dim, n_species, FluxType>;
-  };
-
-  template <int dim, typename FluxType>
-  struct ConcreteFluxViewImpl<dim, 1, FluxType>
-  {
-    using type = FluxView<dim, FluxType>;
-  };
+  using NSpeciesDofValueAndGradientStateView = std::conditional_t<
+    n_species == 1,
+    DofValueAndGradientStateView<dim, number, Value, Gradient>,
+    MultiSpeciesDofValueAndGradientStateView<dim, n_species, number, Value, Gradient>>;
 
   /**
    * Type alias for the FluxView for a specific number of species. This alias resolves to the
    * appropriate flux view type depending on the number of species in the simulation.
    */
   template <int dim, int n_species, typename FluxType>
-  using NSpeciesFluxView = typename ConcreteFluxViewImpl<dim, n_species, FluxType>::type;
+  using NSpeciesFluxView = std::conditional_t<n_species == 1,
+                                              FluxView<dim, FluxType>,
+                                              MultiSpeciesFluxView<dim, n_species, FluxType>>;
 
 } // namespace MeltPoolDG::CompressibleFlow
